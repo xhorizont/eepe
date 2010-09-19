f75a00950e87d1a7ad33316c7d7c5dfc66a2454e
erezraviv <erezraviv@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2010-09-19 17:21:51 +0000

    Project start

---
+++ src/eepe.pro
@@ -0,0 +1,14 @@
+# -------------------------------------------------
+# Project created by QtCreator 2010-09-19T18:26:15
+# -------------------------------------------------
+TARGET = eepe
+TEMPLATE = app
+SOURCES += main.cpp \
+    mainwindow.cpp \
+    pers.cpp \
+    file.cpp
+HEADERS += mainwindow.h \
+    pers.h \
+    myeeprom.h \
+    file.h
+FORMS += mainwindow.ui
---
+++ src/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void on_actionSave_activated();
+    void on_actionOpen_activated();
+    void on_actionQuit_activated();
+};
+
+#endif // MAINWINDOW_H
