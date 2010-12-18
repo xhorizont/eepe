e707dc496b907f31fe97b7e51b6518f9820867ad
erezraviv <erezraviv@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2010-12-18 13:48:58 +0000

    Add er9x Users guide

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
-//Automatically generated file - do not edit  
-#define DATE_STR "13.12.2010" 
-#define TIME_STR "10:41:08" 
-#define SUB_VERS "1-erez" 
-#define SVN_VERS "trunk-r186" 
-#define SVN_VER_NUM 186 
+//Automatically generated file - do not edit
+#define DATE_STR "18.12.2010"
+#define TIME_STR "15:03:38"
+#define SUB_VERS "1-erez"
+#define SVN_VERS "trunk-r190"
+#define SVN_VER_NUM 190
