065488eaa22c1ada85e02d05c6d6a54a9ebebc06
erezraviv@gmail.com <erezraviv@gmail.com@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2010-12-30 09:22:05 +0000

    Templates, Helis stuff and fixes

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
-//Automatically generated file - do not edit
-#define DATE_STR "29.12.2010"
-#define TIME_STR "22:10:08"
-#define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r201"
-#define SVN_VER_NUM 201
+//Automatically generated file - do not edit  
+#define DATE_STR "30.12.2010" 
+#define TIME_STR "11:13:08" 
+#define SUB_VERS "1-erez" 
+#define SVN_VERS "trunk-r202" 
+#define SVN_VER_NUM 202 
