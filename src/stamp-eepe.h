52532b2c63a0cff299b506c2dbd188e143ed7419
erezraviv <erezraviv@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2010-12-31 23:40:47 +0000

    Happy New Year \!\!\!

--- src/stamp-eepe.h
+++ src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "01.01.2011"
-#define TIME_STR "01:01:33"
+#define TIME_STR "01:01:59"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r208"
-#define SVN_VER_NUM 208
+#define SVN_VERS "trunk-r209"
+#define SVN_VER_NUM 209
