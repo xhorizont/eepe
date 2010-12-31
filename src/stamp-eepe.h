0abc58818c9aeaad3799678694ffedcb4240ffe5
erezraviv <erezraviv@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2010-12-31 18:51:35 +0000

    Heli template fix

--- src/stamp-eepe.h
+++ src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "31.12.2010"
-#define TIME_STR "17:05:24"
+#define TIME_STR "20:08:36"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r206"
-#define SVN_VER_NUM 206
+#define SVN_VERS "trunk-r207"
+#define SVN_VER_NUM 207
