64ea16ac7e132a12fdf0fcd05322d62b75912c1e
erezraviv <erezraviv@gmail.com>
2011-05-30 18:48:01 +0000

    New win build

--- src/stamp-eepe.h
+++ src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "19.03.2011"
-#define TIME_STR "08:08:24"
+#define DATE_STR "30.05.2011"
+#define TIME_STR "14:02:18"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r247"
-#define SVN_VER_NUM 247
+#define SVN_VERS "trunk-r249"
+#define SVN_VER_NUM 249
