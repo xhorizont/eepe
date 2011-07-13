be21031157a63709cc7ae45a3c511ad1d1d051d4
erezraviv@gmail.com <erezraviv@gmail.com@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2011-07-13 01:33:03 +0000

    Issue 100

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "26.06.2011"
-#define TIME_STR "02:02:32"
+#define DATE_STR "12.07.2011"
+#define TIME_STR "21:09:15"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r254"
-#define SVN_VER_NUM 254
+#define SVN_VERS "trunk-r255"
+#define SVN_VER_NUM 255
