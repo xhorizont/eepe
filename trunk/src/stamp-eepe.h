965326539d43072a6ecfd45430fa3fdab3503a82
erezraviv@gmail.com <erezraviv@gmail.com@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2011-07-30 02:36:15 +0000

    perout MAX Continue fix

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "28.07.2011"
-#define TIME_STR "22:10:33"
+#define DATE_STR "29.07.2011"
+#define TIME_STR "22:10:08"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r261"
-#define SVN_VER_NUM 261
+#define SVN_VERS "trunk-r262"
+#define SVN_VER_NUM 262
