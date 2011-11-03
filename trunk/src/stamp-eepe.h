41af9c7d0a4de7b0f88b65dea2c345141b88135e
erezraviv@gmail.com <erezraviv@gmail.com@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2011-11-03 22:08:55 +0000

    fixed compile issue 112

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "29.10.2011"
-#define TIME_STR "14:02:06"
+#define DATE_STR "03.11.2011"
+#define TIME_STR "22:10:20"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r279"
-#define SVN_VER_NUM 279
+#define SVN_VERS "trunk-r281"
+#define SVN_VER_NUM 281
