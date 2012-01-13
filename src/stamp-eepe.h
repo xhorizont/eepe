3c12f1904356cac30881e8cff91afe6bd9d26c63
erezraviv <erezraviv@gmail.com>
2012-01-13 03:41:45 +0000

    add linux 32bit and 64 bit deb packages

--- src/stamp-eepe.h
+++ src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "13.01.2012"
-#define TIME_STR "02:02:01"
+#define TIME_STR "03:03:27"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r301"
-#define SVN_VER_NUM 301
+#define SVN_VERS "trunk-r302"
+#define SVN_VER_NUM 302
