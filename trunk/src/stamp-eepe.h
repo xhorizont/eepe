70e9079f224ea6f7c83a863f1bfcfce1d4830d44
erezraviv@gmail.com <erezraviv@gmail.com@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2012-02-04 08:04:18 +0000

    check for update error

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "31.01.2012"
-#define TIME_STR "04:04:14"
+#define DATE_STR "04.02.2012"
+#define TIME_STR "07:07:49"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r322"
-#define SVN_VER_NUM 322
+#define SVN_VERS "trunk-r326"
+#define SVN_VER_NUM 326
