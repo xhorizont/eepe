3e792051f5c66acdaa5bfbb78d40afbeb2aabf01
erezraviv@gmail.com <erezraviv@gmail.com@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2012-02-07 23:23:14 +0000

    Fixed load model and general settings files error, allow revision numbers to be 4 digit

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "04.02.2012"
-#define TIME_STR "07:07:49"
+#define DATE_STR "07.02.2012"
+#define TIME_STR "23:11:27"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r326"
-#define SVN_VER_NUM 326
+#define SVN_VERS "trunk-r328"
+#define SVN_VER_NUM 328
