ee47b6ff315cf508321a6d8bd5827ecc8a3c3235
erezraviv@gmail.com <erezraviv@gmail.com@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2012-09-18 23:45:20 +0000

    build

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "11.09.2012"
-#define TIME_STR "02:02:26"
+#define DATE_STR "18.09.2012"
+#define TIME_STR "23:11:16"
 #define SUB_VERS "1-erez"
-#define SVN_VERS "trunk-r351"
-#define SVN_VER_NUM 351
+#define SVN_VERS "trunk-r353"
+#define SVN_VER_NUM 353
