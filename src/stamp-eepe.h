4ae4b6b1103a9140e378f05d287a5301a4c414b3
michael.blandford <michael.blandford@mypostoffice.co.uk>
2012-10-05 21:04:50 +0000

    Stamp fixed (again)

--- src/stamp-eepe.h
+++ src/stamp-eepe.h
@@ -2,5 +2,5 @@
 #define DATE_STR "05.10.2012"
 #define TIME_STR "16:51:23"
 #define SUB_VERS "1-Mike"
-#define SVN_VERS "trunk-r355"
-#define SVN_VER_NUM 1
+#define SVN_VERS "trunk-r356"
+#define SVN_VER_NUM 356
