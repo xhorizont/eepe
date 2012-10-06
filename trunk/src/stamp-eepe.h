409ef66b79cdc0bbc2afd4fc9fb75f71dfa80e78
michael.blandford@mypostoffice.co.uk <michael.blandford@mypostoffice.co.uk@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2012-10-06 08:20:35 +0000

    Stamp reset to r351, awaiting build

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "05.10.2012"
-#define TIME_STR "16:51:23"
-#define SUB_VERS "1-Mike"
-#define SVN_VERS "trunk-r356"
-#define SVN_VER_NUM 356
+#define DATE_STR "11.09.2012"
+#define TIME_STR "02:02:26"
+#define SUB_VERS "1-erez"
+#define SVN_VERS "trunk-r351"
+#define SVN_VER_NUM 351
