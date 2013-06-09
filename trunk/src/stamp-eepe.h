d8c7a793f5b9d7716995ce58f9ff03595dff04f0
michael.blandford@mypostoffice.co.uk <michael.blandford@mypostoffice.co.uk@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2013-06-09 19:42:40 +0000

    Set stamp to rev 370

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -2,5 +2,5 @@
 #define DATE_STR "07.06.2013"
 #define TIME_STR "22:09:03"
 #define SUB_VERS "1-Mike"
-#define SVN_VERS "trunk-r372"
-#define SVN_VER_NUM 372
+#define SVN_VERS "trunk-r370"
+#define SVN_VER_NUM 370
