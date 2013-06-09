b0a628271f7692f9a6e57c906ec8ea97b73f0226
michael.blandford <michael.blandford@mypostoffice.co.uk>
2013-06-09 12:49:37 +0000

    update stamp

--- src/stamp-eepe.h
+++ src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "24.05.2013"
-#define TIME_STR "19:22:52"
+#define DATE_STR "07.06.2013"
+#define TIME_STR "22:09:03"
 #define SUB_VERS "1-Mike"
-#define SVN_VERS "trunk-r370"
-#define SVN_VER_NUM 370
+#define SVN_VERS "trunk-r372"
+#define SVN_VER_NUM 372
