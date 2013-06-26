cf53cd5b9aeb8bb75240ffa1a2c4dd355bcab3fa
michael.blandford@mypostoffice.co.uk <michael.blandford@mypostoffice.co.uk@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2013-06-26 07:54:52 +0000

    Fix negative expo bug

--- trunk/src/stamp-eepe.h
+++ trunk/src/stamp-eepe.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "23.06.2013"
-#define TIME_STR "14:07:38"
+#define DATE_STR "25.06.2013"
+#define TIME_STR "09:25:27"
 #define SUB_VERS "1-Mike"
-#define SVN_VERS "trunk-r377"
-#define SVN_VER_NUM 377
+#define SVN_VERS "trunk-r378"
+#define SVN_VER_NUM 378
