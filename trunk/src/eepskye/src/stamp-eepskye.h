5cd504aff5382902e3242147835eae51759bd969
michael.blandford@mypostoffice.co.uk <michael.blandford@mypostoffice.co.uk@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2015-07-27 22:57:43 +0000

    Last commit on googlecode, look on www.er9x.com for new revisions

--- trunk/src/eepskye/src/stamp-eepskye.h
+++ trunk/src/eepskye/src/stamp-eepskye.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "26.07.2015"
-#define TIME_STR "22:09:59"
+#define DATE_STR "27.07.2015"
+#define TIME_STR "08:01:18"
 #define SUB_VERS "1-Mike"
-#define SVN_VERS "trunk-r417"
-#define SVN_VER_NUM 417
+#define SVN_VERS "trunk-r418"
+#define SVN_VER_NUM 418
