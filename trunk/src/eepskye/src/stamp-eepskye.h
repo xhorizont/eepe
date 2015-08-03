2184c368972e2c466279879388cd52e6c3147be0
michael.blandford@mypostoffice.co.uk <michael.blandford@mypostoffice.co.uk@9eeb93d8-bee7-963f-4aea-e2c37fe20227>
2015-08-03 09:13:51 +0000

    Fix eepskye update bug

--- trunk/src/eepskye/src/stamp-eepskye.h
+++ trunk/src/eepskye/src/stamp-eepskye.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "27.07.2015"
-#define TIME_STR "08:01:18"
+#define DATE_STR "02.08.2015"
+#define TIME_STR "20:39:33"
 #define SUB_VERS "1-Mike"
-#define SVN_VERS "trunk-r418"
-#define SVN_VER_NUM 418
+#define SVN_VERS "trunk-r420"
+#define SVN_VER_NUM 420
